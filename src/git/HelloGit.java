75511cb3e43ce0ea488e6daf6b62bebe04de29f0
Eric Hay <ehay@mymail.mines.edu>
2020-09-29 16:08:49 -0600

    Second commit

--- src/git/HelloGit.java
+++ src/git/HelloGit.java
@@ -2,6 +2,7 @@ package git;
 
 public class HelloGit {
 	//edit something
+	//edit something again . . . MWAHAHAHAHAHHAHAHAAA!!!
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
